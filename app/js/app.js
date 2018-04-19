1c4d3f90718f99f43c37abd665dde8bc8e0c7952
Felipexox <felipeaa100@gmail.com>
2018-04-19 19:00:40 -0300

    [Commit]   added new actions to js files and united all files

--- app/js/app.js
+++ app/js/app.js
@@ -2,19 +2,21 @@ var app = angular.module('myApp', ["ngRoute"]);
 // rotas são para aplicações que funcionam em uma pagina só
 // no geral é como fazer uma aplicação de multi-telas, mas tem o beneficio de diminuir
 // a quantidade de request necessaria visto que tudo é carregado no inicio
-app.config(function($routerProvider){
+app.config(function($routeProvider){
   //esse lugar é usado para definir as rotas, ou seja, para cada caminho
   //ele retornara um template "o .html" e um controller para aquele template
 
-  $routerProvider
+  $routeProvider
 
-  .when('/',{
-    template : "<h1> ola </h1>",
-    controller : 'homeCtrl',
+  .when("/",{
+    templateUrl : "./partials/home.html",
+    controller : "homeCtrl"
   })
 
-});
+  .when("/sobre",{
+    templateUrl : "./partials/sobre.html",
+    controller : "sobreCtrl"
+  })
 
-app.controller('homeCtrl', function($scope){
 
 });
